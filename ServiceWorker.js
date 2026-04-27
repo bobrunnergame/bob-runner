84e9bac4fb54e477b0ce4c2cb5e92dc2039db4fe
JDapple64 <joshuadavis5543@gmail.com>
2026-04-27 18:58:02 +0100

    Bob

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "JDapple64-Bob Runner-1.07";
-const contentToCache = [
-    "Build/Bob runner web.loader.js",
-    "Build/Bob runner web.framework.js.unityweb",
-    "Build/Bob runner web.data.unityweb",
-    "Build/Bob runner web.wasm.unityweb",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
